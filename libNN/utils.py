fixed batch normalization bug

libNN/utils.py
@@ -41,7 +41,7 @@ class BatchNormalization(object):
         self.mean = np.mean(z, axis = 1, keepdims=True)
         self.var = np.var(z, axis = 1, keepdims=True)
         self.z_ = (z - self.mean)/np.sqrt(self.var + self.epsilon)
-        self.y = self.gamma * z + self.beta
+        self.y = self.gamma * self.z_ + self.beta
         
         return self.y
     
@@ -49,6 +49,7 @@ class BatchNormalization(object):
         """
         return delta loss with respect to original z
         """
+        
         # batch size
         self.m = delta.shape[1]
         
@@ -63,6 +64,7 @@ class BatchNormalization(object):
         # update gamma and beta
         self.gamma -= self.lr * delta_gamma
         self.beta -= self.lr * delta_beta
+        
         
         return delta_z
     

run.py
@@ -9,28 +9,28 @@ from libNN.optimizer import SGD, Adagrad, Adadelta, RMSprop, Adam
 from libNN.utils import BatchNormalization as BN
 
 # load data sets
-train_X = np.load('data/train_X.npy')
-train_y = np.load('data/train_y.npy')
-val_X = np.load('data/val_X.npy')
-val_y = np.load('data/val_y.npy')
-test_X = np.load('data/test_X.npy')
-test_y = np.load('data/test_y.npy')
+train_X = np.load('../data/train_X.npy')
+train_y = np.load('../data/train_y.npy')
+val_X = np.load('../data/val_X.npy')
+val_y = np.load('../data/val_y.npy')
+test_X = np.load('../data/test_X.npy')
+test_y = np.load('../data/test_y.npy')
 
 # model
-model = Network(sizes = [train_X.shape[1], 100, 30, train_y.shape[1]], 
+model = Network(sizes = [train_X.shape[1], 100, 100, train_y.shape[1]], 
                 activations = [Sigmoid(), Sigmoid(), Sigmoid()], 
                 cost = CrossEntropyCost(),
-                batch_normalizations = [None, None, None])
+                batch_normalizations = [BN(), BN(), BN()])
 
 # training
 model.fit(data = train_X[:50000], 
           labels = train_y[:50000], 
-          batch_size = 30, 
-          epochs = 20, 
+          batch_size = 50, 
+          epochs = 30, 
           learning_rate = 0.1, 
           evaluation_data = (val_X, val_y),
           regularizer = L2Regularizer(),
-          _lambda = 0.3,
+          _lambda = 0,
           optimizer = SGD(),
           plot_error = False)
 